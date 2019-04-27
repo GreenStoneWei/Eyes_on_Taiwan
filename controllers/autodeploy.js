051edb83d7c7b36eae6c22c7c7e8e534f5291db9
魏士弘 <stone@weishihongde-MacBook-Pro.local>
2019-04-27 16:32:43 +0800

    fix: autodeploy error handler

--- controllers/autodeploy.js
+++ controllers/autodeploy.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router  = express.Router();
+const myLib   = require('../util/config.js');
 const execFile = require('child_process').execFile;
 
 router.post('/news/auto/payload', (req,res) => {
     execFile('/home/ec2-user/cicdbash/newscraping.sh', (error, stdout, stderr) => {
         if (error){
+            myLib.log(error);
             res.end();
             throw error;
         }

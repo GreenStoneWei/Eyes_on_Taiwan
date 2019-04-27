b042aff3bd15c3cf82bbb451c147afd3fa833574
魏士弘 <stone@weishihongde-MacBook-Pro.local>
2019-04-27 20:44:00 +0800

    test: fb comment webhook

--- controllers/fbcomment.js
+++ controllers/fbcomment.js
@@ -6,7 +6,8 @@ const execFile = require('child_process').execFile;
 
 router.get('/webhook/fb/comment', (req,res) => {
     console.log(req.query);
-    res.send(req.query.hub.challenge);
+    console.log(typeof req.query);
+    res.send(req.query[hub.challenge]);
 })
 
 module.exports = router;

f3c5fa7c1b643d05d45ece32090ad4396be2e8f9
魏士弘 <stone@weishihongde-MacBook-Pro.local>
2019-04-27 20:28:25 +0800

    test: fb comment webhook

--- controllers/fbcomment.js
+++ controllers/fbcomment.js
@@ -4,7 +4,7 @@ const myLib   = require('../util/config.js');
 const mysql   = require('../util/mysql.js');
 const execFile = require('child_process').execFile;
 
-router.post('/webhook/fb/comment', (req,res) => {
+router.get('/webhook/fb/comment', (req,res) => {
     console.log('fb comment');
     res.send('fb')
 })

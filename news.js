7b02e59e8e995a54bd17203bfc27c24c8a96ba89
魏士弘 <stone.wei@wemoscooter.com>
2019-09-14 23:03:17 +0800

    lint

--- news.js
+++ news.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const app = express();
-const credentials = require('./util/credentials.js');
-app.use(express.static('public'));
+const express = require('express')
+const app = express()
+const credentials = require('./util/credentials.js')
+app.use(express.static('public'))
 
 // CORS Control
-app.use('/', function(req, res, next) {
-	res.set('Access-Control-Allow-Origin', '*');
-	res.set('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization');
-	res.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-	res.set('Access-Control-Allow-Credentials', 'true');
-	next();
-});
+app.use('/', function (req, res, next) {
+  res.set('Access-Control-Allow-Origin', '*')
+  res.set('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization')
+  res.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+  res.set('Access-Control-Allow-Credentials', 'true')
+	next()
+})
 
 app.use(require('prerender-node').set('prerenderToken', credentials.myPreRenderToken));
 
